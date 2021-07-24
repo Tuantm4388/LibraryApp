af31d21dad452cd04178997ea49be9b79666b869
tuantm4388 <tuantm4388@yahoo.com>
2021-07-25 02:43:42 +0700

    update search and sort

--- client/src/app/libsystem/lib-members/lib-user-list/lib-user-list.component.ts
+++ client/src/app/libsystem/lib-members/lib-user-list/lib-user-list.component.ts
@@ -27,7 +27,7 @@ export class LibUserListComponent implements OnInit {
   keyWord: string;
 
   constructor(private adminService: AdminService, private modalService: BsModalService,
-     private libUserService: LibUserService, private router: Router) { }
+    private libUserService: LibUserService, private router: Router) { }
 
   ngOnInit(): void {
     this.getUsersWithRoles();
@@ -45,38 +45,51 @@ export class LibUserListComponent implements OnInit {
       class: 'modal-dialog-centered',
       initialState: {
         user,
-        success:false
+        success: false
       }
     }
     this.bsModalRef = this.modalService.show(LibDeleteUserComponent, config);
     this.bsModalRef.content.updateAction.subscribe(values => {
-      if(values){
+      if (values) {
         let idx = this.users.indexOf(user);
-        this.users.splice(idx,1);
-        this.usersBackup.splice(idx,1);
+        this.users.splice(idx, 1);
+        this.usersBackup.splice(idx, 1);
       }
     });
   }
 
-  routToEditPage(user:LibUser)
-  {
+  routToEditPage(user: LibUser) {
     this.libUserService.setSelectedUserInfo(user);
-    this.router.navigateByUrl('/edituser/'+user.username);
+    this.router.navigateByUrl('/edituser/' + user.username);
   }
 
   searchFunction() {
     if (this.keyWord && this.keyWord.length > 0) {
-      let resulf = this.usersBackup.find(a => a.username.toUpperCase() == this.keyWord.toUpperCase());
+      //let resulf = this.usersBackup.find(a => a.username.toUpperCase() == this.keyWord.toUpperCase());
+      let resulf = this.usersBackup.filter(a => this.isSearchCompare(a.username, this.keyWord));
       console.log(resulf);
       this.users = [];
       if (resulf) {
         console.log("searchFunction added");
-        this.users.push(resulf);
+        this.users = resulf;
       }
+      this.p = 1;
     }
     else {
       this.users = this.usersBackup;
+      this.p = 1;
     }
+  }
+
+  isSearchCompare(_strParent: string, _strChild: string) {
+    let strParent: string = _strParent.toLowerCase();
+    let strChild: string = _strChild.toLowerCase();
+    let a = strParent.indexOf(strChild);
+    /*if (a == -1) {
+    } else {
+    }*/
+    if (a > -1) return true;
+    return false;
   }
 
   /// tablesorter
@@ -97,7 +110,7 @@ export class LibUserListComponent implements OnInit {
     if (this.sorterName) {
       this.sortToLow_Name();
     }
-    else{
+    else {
       this.sortToUp_Name();
     }
   }
@@ -132,7 +145,48 @@ export class LibUserListComponent implements OnInit {
     });
   }
 
+  sorterType: boolean = true;
+  sort_Type() {
+    this.sorterType = !this.sorterType;
+    if (this.sorterType) {
+      this.sortToLow_Type();
+    }
+    else {
+      this.sortToUp_Type();
+    }
+  }
+
+  sortToLow_Type() {
+    this.users = this.usersBackup.sort(function (a, b) {
+      var nameA = a.roles; // ignore upper and lowercase
+      var nameB = b.roles; // ignore upper and lowercase
+      if (nameA < nameB) {
+        return 1;
+      }
+      if (nameA > nameB) {
+        return -1;
+      }
+      // must be equal
+      return 0;
+    });
+  }
+
+  sortToUp_Type() {
+    this.users = this.usersBackup.sort(function (a, b) {
+      var nameA = a.roles; // ignore upper and lowercase
+      var nameB = b.roles; // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      // must be equal
+      return 0;
+    });
+  }
+
   ///////
-  
+
 
 }
